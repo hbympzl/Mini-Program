ba1a5e55f09dcdd12a408e66606a5b7c4b3b4281
pzlc <hbympzl@qq.com>
2019-07-03 16:59:04 +0800

    2019.7.3

--- ggg/pages/index/index.js
+++ ggg/pages/index/index.js
@@ -2,152 +2,137 @@
 //获取应用实例
 const app = getApp();
 var score = 0;
-var total_score = 0; 
+var total_score = 0;
 var util = require('../../utils/util.js');
 var time = util.formatTime(new Date());
-
 // 再通过setData更改Page()里面的data，动态更新页面的数据
 Page({
   data: {
     score: score
   },
   //加一后存储到缓存中
-  addition: function (e) {
+  addition: function(e) {
+    var that = this;
     var todayScore = wx.getStorageSync('today_score');
     var addScore = todayScore + 1;
-     this.setData({
-       score: addScore
-     })
+    that.allScore('score', addScore);
     wx.setStorageSync('today_score', addScore);
   },
-    //减一后存储到缓存中
-  subtraction: function (e) {
+  //减一后存储到缓存中
+  subtraction: function(e) {
+    var that = this;
     var todayScore = wx.getStorageSync('today_score');
     var subScore = todayScore - 1;
-    this.setData({
-      score: subScore
-    })
+    that.allScore('score', subScore);
     wx.setStorageSync('today_score', subScore);
   },
   //初始加载项
-  onLoad: function () {
+  onLoad: function() {
     //创建缓存
-    var that = this; 
+    var that = this;
     that.initCache('today_score');
     that.initCache('total_score');
     var todayScore = wx.getStorageSync('today_score');
-    this.setData({
-      score: todayScore
-    })  
+    that.allScore('score', todayScore);
     var totalScore = wx.getStorageSync('total_score');
-    that.allScore(totalScore);
+    that.allScore('total_score', totalScore);
     // 调用函数时，传入new Date()参数，返回值是日期和时间
     //计时器每隔一段时间进行时间确认
-    var interval = setInterval(function () {
+    var interval = setInterval(function() {
       time = util.formatTime(new Date());
       that.timeGoesBy(time)
       if (time.substring(11, 20) == '00:00:00') {
-      //if(false){
+        //if(false){
         //每天12点的时候进行数据上传,直接上传到缓存当中
         //取出缓存
         totalScore = wx.getStorageSync("total_score");
         todayScore = wx.getStorageSync('today_score');
         //缓存与现有数据相加
-        wx.setStorageSync('total_score', totalScore+todayScore);
+        wx.setStorageSync('total_score', totalScore + todayScore);
         totalScore = wx.getStorageSync("total_score");
-        that.allScore(totalScore);
+        that.allScore('total_score', totalScore);
         wx.setStorageSync('today_score', 0);
-        that.setData({
-          score: 0
-        }) 
+        that.allScore('score', 0);
       }
     }, 1000)
   },
-  timeGoesBy: function(e){
+  timeGoesBy: function(e) {
+    var that = this;
+    that.allScore('now_time', e);
+  },
+  //设置页面参数的公共方法
+  allScore: function(key, value) {
     this.setData({
-     now_time:e
+      [key]: value
     })
   },
-  allScore:function(e){
-    this.setData({
-      total_score:e
-    })
-  },
-  initCache:function(e){
+  initCache: function(e) {
     var isExist = wx.getStorageSync(e);
-    if (isExist == ""){
+    if (isExist == "") {
       wx.setStorage({
         key: e,
         data: 0,
       })
     }
   },
-  synScore:function(e){
+  synScore: function(e) {
     var that = this;
-    if((that.data.touchEnd-that.data.touchStart)>= 3000){
+    if ((that.data.touchEnd - that.data.touchStart) >= 3000) {
       var totalScore = wx.getStorageSync('today_score');
       wx.setStorageSync('total_score', totalScore);
       this.onLoad();
     }
   },
-  bindTouchStart: function (e) {
+  bindTouchStart: function(e) {
     var that = this;
-    that.setData({
-      touchStart: e.timeStamp
-    })  
+    that.allScore('touchStart', e.timeStamp);
   },
-  bindTouchEnd: function (e) {
+  bindTouchEnd: function(e) {
     var that = this;
-    that.setData({
-      touchEnd: e.timeStamp
-    })  
+    that.allScore('touchEnd', e.timeStamp);
+
   },
-  additionplus:function(e){
+  additionplus: function(e) {
     var that = this;
     var todayScore = wx.getStorageSync('today_score');
     var addScore = todayScore + 1;
-    this.setData({
-      score: addScore
-    })
+    that.allScore('score', addScore);
     wx.setStorageSync('today_score', addScore);
-    that.setData({
-      addFlag:1
-    }) 
-    setTimeout(function () {
-        if(that.data.addFlag == 1){
-            that.additionplus();
-        }
+    that.allScore('addFlag', 1);
+    setTimeout(function() {
+      if (that.data.addFlag == 1) {
+        that.additionplus();
+      }
     }, 50)
- },
-  endAdditionplus:function(e){
-    var that = this;
-    that.setData({
-      addFlag:0
-    })
   },
-  subtractionplus: function (e) {
+  endAdditionplus: function(e) {
+    var that = this;
+    that.allScore('addFlag', 0);
+  },
+  subtractionplus: function(e) {
     var that = this;
     var todayScore = wx.getStorageSync('today_score');
     var subScore = todayScore - 1;
-    this.setData({
-      score: subScore
-    })
+    that.allScore('score', subScore);
     wx.setStorageSync('today_score', subScore);
-    that.setData({
-      subFlag: 1
-    })
-    setTimeout(function () {
+    that.allScore('subFlag', 1);
+    setTimeout(function() {
       if (that.data.subFlag == 1) {
         that.subtractionplus();
       }
     }, 50)
   },
-  endsubtractionplus: function (e) {
+  endsubtractionplus: function(e) {
     var that = this;
-    that.setData({
-      subFlag: 0
-    })
+    that.allScore('subFlag', 0);
+  },
+  onShow: function() {
+    var that = this;
+    console.log("切换到了index页面");
+    var todayScore = wx.getStorageSync('today_score');
+    that.allScore('score', todayScore);
+    var totalScore = wx.getStorageSync('total_score');
+    that.allScore('total_score', totalScore);
   }
 
 })
- 
--- ggg/pages/reward/reward.js
+++ ggg/pages/reward/reward.js
@@ -4,8 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    whatwewant: [
-      {
+    whatwewant: [{
         name: "日本任天堂（Nintendo）便携掌上游戏机 Switch NS主机 红蓝手柄 日版",
         score: 1000,
         imagePath: "/images/goods/ns.jpg",
@@ -15,28 +14,35 @@ Page({
         name: "LOL至臻皮肤门票",
         score: 30,
         imagePath: "/images/goods/zz.png",
-        id: 19941016
+        id: 199410162
       },
+      /*{
+        name: "测试",
+        score: 1,
+        imagePath: "/images/goods/test.png",
+        id: 199410163
+      },*/
     ]
   },
-  exchangeGoods:function(e){
-    var exchangeId =  e.currentTarget.dataset.whatwewantId;
+  exchangeGoods: function(e) {
+    var exchangeId = e.currentTarget.dataset.whatwewantId;
     var totalScore = wx.getStorageSync("total_score");
     var that = this;
     var goods = that.data.whatwewant;
-    for (var i = 0; i< goods.length; i ++){
-      if (goods[i].id == exchangeId){
-        if (totalScore >= goods[i].score){
+    for (var i = 0; i < goods.length; i++) {
+      if (goods[i].id == exchangeId) {
+        if (totalScore >= goods[i].score) {
+          wx.setStorageSync("total_score", totalScore - goods[i].score);
           wx.showToast({
             title: '兑换成功',
           })
-
-        }else{
-          if (typeof (goods[i].score) == 'string'){
+          goods[i].score = "已兑换";
+        } else {
+          if (typeof(goods[i].score) == 'string') {
             wx.showToast({
               title: '已经兑换过了',
             })
-          }else{
+          } else {
             wx.showToast({
               title: '积分不足',
             })
@@ -44,6 +50,5 @@ Page({
         }
       }
     }
-   
-  }
+  },
 })
