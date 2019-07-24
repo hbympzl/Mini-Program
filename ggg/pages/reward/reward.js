f56ba799be7af6d2a58b706b5cebd0e3c980c583
pzlc <hbympzl@qq.com>
2019-07-24 15:16:13 +0800

    2019.7.24 新增完成任务增加积分功能

--- ggg/pages/reward/reward.js
+++ ggg/pages/reward/reward.js
@@ -24,16 +24,22 @@ Page({
         id: 199410164
       }, 
       {
-        name: "LOL至臻皮肤门票",
-        score: '已兑换',
-        imagePath: "/images/goods/zz.png",
-        id: 199410162
+        name: "JAVA基础视频",
+        score: '+20',
+        imagePath: "/images/goods/javabasic.png",
+        id: 199410165
+      }, 
+      {
+        name: "JQuery自定义插件",
+        score: '+30',
+        imagePath: "/images/goods/jquery.png",
+        id: 199410166
       }, 
       /*{
         name: "测试",
         score: 1,
         imagePath: "/images/goods/test.png",
-        id: 199410163
+        id: 199410167
       },*/
     ]
   },
@@ -47,8 +53,20 @@ Page({
     for (var i = 0; i < goods.length; i++) {
       //找到商品进行处理
       if (goods[i].id == exchangeId) {
+        //加分项
+        if (typeof (goods[i].score) == 'string'&&goods[i].score.substring(0, 1) == '+') {
+          console.log(1)
+          var add = parseInt(goods[i].score.substring(1));
+          wx.vibrateLong({});
+          wx.setStorageSync("total_score", totalScore + add);
+          wx.showToast({
+            title: '兑换成功',
+            image: '/images/goods/enough.png'
+          })
+          goods[i].score = "已兑换";
+        }
         //将商品需要的分数与现有总数进行对比
-        if (totalScore >= goods[i].score) {
+        else if (totalScore >= goods[i].score) {
           wx.vibrateLong({});
           wx.setStorageSync("total_score", totalScore - goods[i].score);
           wx.showToast({
@@ -56,21 +74,20 @@ Page({
             image:'/images/goods/enough.png'
           })
           goods[i].score = "已兑换";
-        } else {
-          if (typeof(goods[i].score) == 'string') {
-            wx.vibrateLong({});
-            wx.showToast({
-              title: '已经兑换过了',
-              image: '/images/goods/exchanged.png'
-            })
-          } else {
+        } else if (typeof (goods[i].score) == 'string' && goods[i].score.substring(0, 1) != '+'){
+              wx.vibrateLong({});
+              wx.showToast({
+                title: '已经兑换过了',
+                image: '/images/goods/exchanged.png'
+              })
+            
+          }else {
             wx.vibrateLong({});
             wx.showToast({
               title: '积分不足',
               image:'/images/goods/lack.png'
             })
           }
-        }
       }
     }
   },
